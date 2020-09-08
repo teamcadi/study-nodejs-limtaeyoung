61a5b6d8678ef3994458c157861cb302cce11eed
taeyoung98 <119taeyoung@naver.com>
2020-09-08 23:26:45 +0900

    node study 2nd

---
+++ week2/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// 정적 파일 제공하는 api
+
+// 상대경로
+// app.use("/public", express.static("public")); //어플리케이션레벨(전체)에서 미들웨어를 등록하는 메소드
+
+// 1
+app.use(express.json()); // -> (req,res,next) => { //로직; next(); }
+// 2
+app.use(express.urlencoded({ extended: true }));
+// 3 절대경로
+app.use("/public", express.static(path.join(__dirname, "public")));
+// 4
+app.post("/", (req, res) => {
+  const data = req.body;
+  console.log(data.name);
+  // 디비 유저 생성
+  // 수정 etc
+});
+
+// app이 많은 기능을 포함하고 서버를 열었다.
+app.listen(9000, () => {
+  console.log("서버 실행 중");
+});
---
+++ week2/app2.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+// const http = require("http").Server(app);
+/*
+http
+- app api
+- socket 양방향 통신
+*/
+
+// // 어플리케이션 레벨에서 미들웨어 등록
+// app.use((req, res, next) => {
+//   console.log("미들웨어 동작");
+//   //   next(); // if 주석처리, 라우터로 가지 않음. so "조회" 뜨지 않음.
+// });
+
+// 로거 만들기
+// 클라이언트에서 요청이 왔을 때, 응답 여부를 남겨서 로그를 수집하는 것.
+// app.use((req, res, next) => {
+//   next(); // 순서1
+//   const date = new Date(); //
+//   const method = req.method;
+//   const uri = req.originalUrl;
+//   const ip = req.ip;
+
+//   console.log(`${method} ${uri} ${ip} ${date}`); //순서2
+// });
+app.use(morgan("dev"));
+
+// api 구현
+app.get("/", (req, res, next) => {
+  console.log("api");
+  res.send("비밀 데이터");
+});
+
+app.listen(9001, () => {
+  console.log("9001 실행중");
+});
